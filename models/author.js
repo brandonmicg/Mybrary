7b7fd3ea67ed7852d1d728f350d6f8b433faf980
Brandon Girard <branmic11@gmail.com>
2021-12-14 19:38:44 -0500

    Finished author routes

--- models/author.js
+++ models/author.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
+const book = require('./book')
 
 const authorsSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     }
+})
+
+authorsSchema.pre('remove', function(next) {
+    Book.find({ author: this.id }, (err, books) => {
+        if(err){
+            next(err)
+        }else if(book.length > 0){
+            next(new Error('This author has books still'))
+        }else{
+            next()
+        }
+    })
 })
 
 module.exports = mongoose.model('Author', authorsSchema)
